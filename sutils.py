b4deabae0daa61f74aff095ff75193a1ca959c7e
Sean McKinney <jouyun@gmail.com>
2024-02-02 14:58:59 -0600

    Chapter3 Update

--- sutils.py
+++ sutils.py
@@ -147,3 +147,36 @@ def shrink_labels(label_image, shrinkage=3):
         shrunken_label_image[minr:maxr, minc:maxc][eroded_region_mask == 1] = lbl
 
     return shrunken_label_image
+
+def download_images(url, extract_to):
+    '''
+    From the github gist
+    https://gist.github.com/hantoine/c4fc70b32c2d163f604a8dc2a050d5f6
+    '''
+    http_response = urlopen(url)
+    zipfile = ZipFile(BytesIO(http_response.read()))
+    zipfile.extractall(path=extract_to)
+    
+
+#def checkdata_md5():
+#    url = 
+        
+def setlogging():
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    root.addHandler(handler)
+
+def check(gpu=True, device='mps'):
+    model = models.Cellpose(gpu=gpu, device=device)
+    return model
+ 
+def checkmps():   
+    d = torch.device('mps')    
+    model = check(gpu=True, device=d)
+    print(model.device)
+    
